835d70e3933c13d597aa68bbfaaaf07eec7057d6
Michael Mallari <hello@michaelmallari.com>
2018-09-07 19:35:47 -0400

    vue init webpack vue-client

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// Vue app build
+app.use('/community', express.static(__dirname + '/vue-client/build'));
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
